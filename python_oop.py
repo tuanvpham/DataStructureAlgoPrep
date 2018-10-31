1925a7d05a56e65a477f39e037b64c626386c3fe
Tuan Pham <tuanvpham32@outlook.com>
2018-10-31 18:05:41 -0400

    work on subclasses

--- python_oop.py
+++ python_oop.py
@@ -17,9 +17,41 @@ class Employee:
 		self.pay = int(self.pay * self.raise_amount)
 
 
+class Developer(Employee):
+	raise_amt = 1.10
+
+	def __init__(self, first, last, pay, prog_lang):
+		# super init: pass first, last, pay to superclass's init method
+		super().__init__(first, last, pay)
+		self.prog_lang = prog_lang
+
+
+class Manager(Employee):
+
+	def __init__(self, first, last, pay, employees=None):
+		super.__init__(first, last, pay)
+		if employees is None:
+			self.employees = []
+		else:
+			self.employees = employees
+
+	def add_emp(self, emp):
+		if emp not in self.employees:
+			self.employees.append(emp)
+
+	def remove_emp(self, emp):
+		if emp in self.employees:
+			self.employees.remove(emp)
+
+	def print_emps(self):
+		for emp in self.employees:
+			print('--->', emp.fullname())
+
 
 emp_1 = Employee('Kevin', 'Pham', 50000)
 emp_2 = Employee('Test', 'User', 60000)
+
+dev_1 = Developer('Tuan', 'Pham', 23000, 'Python')
 
 
 print(emp_1.fullname())
@@ -30,3 +62,7 @@ print(Employee.fullname(emp_1))
 print(Employee.raise_amount)
 print(emp_1.raise_amount)
 print(emp_2.raise_amount)
+
+print(dev_1.email)
+
+
