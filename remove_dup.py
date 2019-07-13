fbebe32c71843e3aebec16001102bea9183d9bdb
Tuan Pham <tuanvpham32@outlook.com>
2019-07-13 15:46:10 -0400

    problem does not mention about not removing elements

--- remove_dup.py
+++ remove_dup.py
@@ -1,3 +1,11 @@
+# https: // leetcode.com/problems/remove-duplicates-from-sorted-array/
+# Sample: [0,0,1,1,1,2,2,3,3,4]
+
+# This solution requires us to return length of sorted array without duplication and modify the given array without allocating new array, but allow to have O(1) extra memory
+# Hence, I used dictionary to look up the numbers, remove duplication in given array, and return the length of modified array
+# However, other people's solutions do not remove duplication, but use an variable to count
+# Do we really need to remove the duplication as the problem told?
+
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         look_up_table = {}
@@ -12,3 +20,25 @@ class Solution:
                 for i in range(v-1):
                     nums.remove(k)
         return len(nums)
+
+# nums arr: [0,1,2,3,4]
+# result: array length
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums) < 1:
+            return 0
+        idx = 1
+        last_num = nums[0]
+        for i in range(1, len(nums)):
+            if last_num != nums[i]:
+                last_num = nums[i]
+                nums[idx] = last_num
+                idx += 1
+            print(nums)
+        return idx
+
+# nums arr: [0,1,2,3,4,2,2,3,3,4]
+# idx: 5
+# result: count idx
